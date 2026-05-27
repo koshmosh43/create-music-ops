97ffe3fb72062ba031e5dc144e950a90b2273042
vlada.cosmoss <vlada.cosmoss@gmail.com>
2026-05-27 17:08:26 -0600

    refactoring: deploy build versioning update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/create-music-ops/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
